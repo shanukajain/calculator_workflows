0be7c051a1a42b44cb36627bbfa578827523b8a8
shanukajain <jain.shanuka@gmail.com>
2023-02-18 21:20:06 +0530

    calculator

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ switch (operation) {
         console.log("should return message if no length is given for random");
         return;
       }
-      console.log(buf.toString("hex"));
+      console.log(buf.toString());
       return 
   });
 break;
